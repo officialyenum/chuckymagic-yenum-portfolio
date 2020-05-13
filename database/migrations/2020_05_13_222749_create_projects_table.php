711253ac40156c94b25d4fdb6dd4e3008a2dd3af
Opone Chukwuyenum <oponechukwuyenum@icloud.com>
2020-05-14 00:10:18 +0100

    first wave commit

---
+++ database/migrations/2020_05_13_222749_create_projects_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProjectsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('projects', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->string('description');
+            $table->text('content');
+            $table->string('image');
+            $table->integer('category_id');
+            $table->integer('user_id');
+            $table->string('github_url')->nullable();
+            $table->string('playstore_url')->nullable();
+            $table->string('appstore_url')->nullable();
+            $table->string('web_url')->nullable();
+            $table->timestamp('published_at')->nullable();
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('projects');
+    }
+}
